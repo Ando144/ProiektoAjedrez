bb1408d90a3eea0f5c4836646b82354574a97ecb
Unairc23 <unairc23@gmail.com>
2023-04-18 20:49:06 +0200

    [Zaldia] igual funciona igual no
    He hecho asi lo basico del caballo, imagino que ira bien pero no he mirado cosas todavia

--- PmoboProiektua/src/xakea/Zaldia.java
+++ PmoboProiektua/src/xakea/Zaldia.java
@@ -9,7 +9,31 @@ public class Zaldia extends Pieza {
     @Override
     public boolean mugituDaiteke(int zeinErrenkadara, int zeinZutabera) {
     	boolean ahalDu = true;
-    	//...
-    	return ahalDu;
+    	//comprueba a ver si la casilla a la que se va a mover esta dentro del tablero
+        if(!Taula.getTaula().laukiaBetetaDago(zeinErrenkadara, zeinZutabera)){
+            ahalDu=false;
+        }
+        
+        if(ahalDu!=false){
+            //El caballo se mueve en una L de 2x3, o vamos si se mueve una errenkada para arriba o para abajo (da igual) 
+            //tiene que moverse tambien dos zutabe para la izquieda o derecha 
+            if(this.getErrenkada()==zeinErrenkadara-1||this.getErrenkada()==zeinErrenkadara+1){
+                if(this.getZutabea()==zeinZutabera-2||this.getZutabea()==zeinZutabera+2){
+                    return ahalDu;
+                }
+            }
+            //Lo mismo al contrario, si se mueve una zutabe a la izquierda o derecha se mueve tambien dos errenkada
+            //arriba o abajo
+            if(this.getZutabea()==zeinZutabera-1||this.getZutabea()==zeinZutabera+1){
+                if(this.getErrenkada()==zeinErrenkadara-2||this.getErrenkada()==zeinErrenkadara+2){
+                    return ahalDu;
+                }
+            }
+            else{
+                ahalDu=false;
+            }
+        }
+        
+        return ahalDu;
     }
 }

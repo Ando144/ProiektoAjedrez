b695ffd8507d5d51f06d5a4119785c524921abe1
Ando144 <andoni.otzdezarate@gmail.com>
2023-04-21 18:08:36 +0200

    [Dorrea][Peoia] Unos cambios y peoia
    He terminado el mugitu daiteke de la dorrea, todavia faltaria el enroque y tal.
    Ademas he empezado el peoia

--- PmoboProiektua/src/xakea/Peoia.java
+++ PmoboProiektua/src/xakea/Peoia.java
@@ -9,7 +9,39 @@ public class Peoia extends Pieza {
     @Override
     public boolean mugituDaiteke(int zeinErrenkadara, int zeinZutabera) {
     	boolean ahalDu = true;
-    	//...
+
+        /*Comprobador de si la pieza es de su mismo color */
+		if (super.zuriaDa())
+		{
+			/*La pieza es blanca y quiere moverse a un sitio con una pieza blanca */
+			if (Taula.getTaula().laukiaBetetaDago(zeinErrenkadara, zeinZutabera) && Taula.getTaula().getLaukikoPieza(zeinErrenkadara, zeinZutabera).zuriaDa())
+			{
+				ahalDu=false;
+			}
+		}
+		else
+		{	/*La pieza es negra y quiere moverse a un sitio con una pieza negra */
+			if (Taula.getTaula().laukiaBetetaDago(zeinErrenkadara, zeinZutabera) && !Taula.getTaula().getLaukikoPieza(zeinErrenkadara, zeinZutabera).zuriaDa())
+			{
+				ahalDu=false;
+			}
+		}
+
+        /*Comprueba si esta dentro del tablero a donde quiere  ir */
+		if (!Taula.getTaula().laukiaTaulanDago(zeinErrenkadara, zeinZutabera))
+		{
+			ahalDu=false;
+		}
+
     	return ahalDu;
+
+        if(ahalDu==false){
+        
+            if(Taula.getTaula().getLaukikoPieza(zeinErrenkadara, zeinZutabera).zuriaDa())
+            {
+                d
+            }
+
+        }
     }
 }

1018a671e9b04cac7d21b4ef4a74eb6b372be4d2
Unairc23 <unairc23@gmail.com>
2023-05-03 19:41:01 +0200

    [Zaldia] arreglado

--- PmoboProiektua/src/xakea/Zaldia.java
+++ PmoboProiektua/src/xakea/Zaldia.java
@@ -2,35 +2,36 @@ package xakea;
 
 public class Zaldia extends Pieza {
 	
-    public Zaldia(boolean kolorea, int errenkada, int zutabea, String izena) {
+    public Zaldia(boolean kolorea, int errenkada, int zutabea, String izena) 
+	{
         super(kolorea, errenkada, zutabea, izena);
     }
 
     @Override
     public boolean mugituDaiteke(int zeinErrenkadara, int zeinZutabera) {
-    	boolean ahalDu = true;
+        System.out.println("malmal"); 
+        boolean ahalDu = true;
     	//comprueba a ver si la casilla a la que se va a mover esta dentro del tablero
-        if(!Taula.getTaula().laukiaBetetaDago(zeinErrenkadara, zeinZutabera)){
+        if(!Taula.getTaula().laukiaTaulanDago(zeinErrenkadara, zeinZutabera)){
             ahalDu=false;
         }
         
         if(ahalDu!=false){
+            ahalDu=false;
             //El caballo se mueve en una L de 2x3, o vamos si se mueve una errenkada para arriba o para abajo (da igual) 
             //tiene que moverse tambien dos zutabe para la izquieda o derecha 
-            if(this.getErrenkada()==zeinErrenkadara-1||this.getErrenkada()==zeinErrenkadara+1){
-                if(this.getZutabea()==zeinZutabera-2||this.getZutabea()==zeinZutabera+2){
-                    return ahalDu;
+            if((this.getErrenkada()==zeinErrenkadara-1)||(this.getErrenkada()==zeinErrenkadara+1)){
+                if((this.getZutabea()==zeinZutabera-2||this.getZutabea()==zeinZutabera+2)){
+                    //Comprueba que el cuadrado este vacio o la pieza sea del color contrario
+                    ahalDu=true;
                 }
             }
             //Lo mismo al contrario, si se mueve una zutabe a la izquierda o derecha se mueve tambien dos errenkada
             //arriba o abajo
-            if(this.getZutabea()==zeinZutabera-1||this.getZutabea()==zeinZutabera+1){
-                if(this.getErrenkada()==zeinErrenkadara-2||this.getErrenkada()==zeinErrenkadara+2){
-                    return ahalDu;
+            if((this.getZutabea()==zeinZutabera-1||this.getZutabea()==zeinZutabera+1)){
+                if((this.getErrenkada()==zeinErrenkadara-2||this.getErrenkada()==zeinErrenkadara+2)){
+                    ahalDu=true;
                 }
-            }
-            else{
-                ahalDu=false;
             }
         }
         

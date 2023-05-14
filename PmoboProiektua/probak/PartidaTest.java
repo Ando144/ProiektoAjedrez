0ccc9fba8007c259a28b253a6480108c715df30e
asierfg794 <asierfaraig@gmail.com>
2023-05-14 20:09:27 +0200

    PartidaTest lo que he podido

--- PmoboProiektua/probak/PartidaTest.java
+++ PmoboProiektua/probak/PartidaTest.java
@@ -4,49 +4,47 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import xakea.*;
+
 public class PartidaTest {
+
+    Partida p1,p2;
 
 	@Before
 	public void setUp() throws Exception {
+        p2 = new Partida();
 	}
 
 	@After
 	public void tearDown() throws Exception {
+        
+        Taula.getTaula().reset();
 	}
 
 	@Test
-	public void testPartidaBatJolastu() {
-		fail("Not yet implemented");
+	public void testPartida() {
+		assertNull(p1);
+        p1=new Partida();
+        assertNotNull(p1);
 	}
 
 	@Test
 	public void testPartidaBukatuDa() {
-		fail("Not yet implemented");
+		p2.partidaBatJolastu(); //Partida jolastu bukatu arte (xake mate lortu edo "bai" idatzi berdinketa gertatu arte)
+		assertTrue(p2.partidaBukatuDa());
 	}
 
 	@Test
 	public void testXakeMate() {
-		fail("Not yet implemented");
+		p2.partidaBatJolastu(); //Partida jolastu xake mate lortu arte
+		assertTrue(p2.partidaBukatuDa());
+		assertFalse(p2.berdinketa());
 	}
 
 	@Test
 	public void testBerdinketa() {
-		fail("Not yet implemented");
+		p2.partidaBatJolastu(); //Berdinketa adostu ("bai" idatzi berdinketa gertatu arte)
+		assertTrue(p2.partidaBukatuDa());
+		assertTrue(p2.berdinketa());
 	}
-
-	@Test
-	public void testMugimenduBatEgin() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testJokalarizAldatu() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testEmaitzaInprimatu() {
-		fail("Not yet implemented");
-	}
-
 }
